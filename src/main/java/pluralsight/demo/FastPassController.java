cd860e744096c876acc149d739bb66c11452b837
trini <trinivazquezfernandez84@gmail.com>
2020-10-29 10:22:55 +0100

    Before state

--- src/main/java/pluralsight/demo/FastPassController.java
+++ src/main/java/pluralsight/demo/FastPassController.java
@@ -1,9 +1,10 @@
 package pluralsight.demo;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,35 +12,40 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.loadbalancer.AbstractLoadBalancer;
+
+@RibbonClient(name="pluralsight-fastpass-service", configuration=TollClientRoutingConfig.class)
 @Controller
 public class FastPassController {
-
+	
 	@LoadBalanced
 	@Bean
-	public RestTemplate restTemplate(RestTemplateBuilder builder){
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
 		return builder.build();
 	}
-
+	
 	@Autowired
 	private RestTemplate restTemplate;
-
-	@HystrixCommand(fallbackMethod = "GetFastPassCustomerDetailsBackup")
+	
+	@HystrixCommand(fallbackMethod="getFastPassCustomerDetailsBackup")
 	@RequestMapping(path="/customerdetails", params={"fastpassid"})
 	public String getFastPassCustomerDetails(@RequestParam String fastpassid, Model m) {
-
+		
 		FastPassCustomer c = restTemplate.getForObject("http://pluralsight-fastpass-service/fastpass?fastpassid=" + fastpassid, FastPassCustomer.class);
 		System.out.println("retrieved customer details");
 		m.addAttribute("customer", c);
+		
 		return "console";
 	}
-
-	public String GetFastPassCustomerDetailsBackup(@RequestParam String fastpassid, Model m){
+	
+	public String getFastPassCustomerDetailsBackup(@RequestParam String fastpassid, Model m) {
+		
+		FastPassCustomer c = new FastPassCustomer();
+		c.setFastPassId(fastpassid);
 		System.out.println("Fallback operation called");
-		FastPassCustomer customer = new FastPassCustomer();
-		customer.setFastPassId(fastpassid);
-		m.addAttribute("customer", customer);
+		m.addAttribute("customer", c);
 		return "console";
-
 	}
 
 }
